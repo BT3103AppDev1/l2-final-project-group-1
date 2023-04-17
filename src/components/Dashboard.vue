39009c156c957678ad44c6f33d83d19d3ea252aa
mauvedelune <kyrialjz@gmail.com>
2023-04-17 14:58:56 +0800

    update settings.vue

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,9 +1,9 @@
 <template> 
   <header>
     <p id="projectTitle">{{ projectTitle }}</p> 
-    <p v-if="this.risk === 0" class="riskStatus" id="low"><strong style="font-weight: 500;">Your Project is On Track</strong></p>
-    <p v-if="this.risk === 1" class="riskStatus" id="slight"><strong style="font-weight: 500;">Your Project is Slightly at Risk</strong></p>
-    <p v-if="this.risk === 2" class="riskStatus" id="high"><strong style="font-weight: 500;">Your Project is at Risk</strong></p>
+    <p v-if="this.risk === 0" class="riskStatus" id="low"><strong style="font-weight: 500;font-size:20px;">Your Project is On Track</strong></p>
+    <p v-if="this.risk === 1" class="riskStatus" id="slight"><strong style="font-weight: 500;font-size:20px;">Your Project is Slightly at Risk</strong></p>
+    <p v-if="this.risk === 2" class="riskStatus" id="high"><strong style="font-weight: 500;font-size:20px;">Your Project is at Risk</strong></p>
   </header>
   <div class="chart-container">
     <div id="chart1">
